f4220df7761361cca3936d76182629a815512727
yurii-bezus <bylchenok@gmail.com>
2025-05-14 12:17:15 +0300

    up

--- js/blended1/lesson-2.js
+++ js/blended1/lesson-2.js
@@ -90,14 +90,141 @@
 // збережіть його результат в змінній sum.
 // Якщо об'єкт salaries пустий, то результат має бути 0
 
-const salaries = {
-  Mango: 100,
-  Poly: 160,
-  Ajax: 1470,
-};
+// const salaries = {
+//   Mango: 100,
+//   Poly: 160,
+//   Ajax: 1470,
+// };
 
-let sum = 0;
-for (const salary of Object.values(salaries)) {
-  sum += salary;
-}
-console.log("загальна сумма", sum);
+// let sum = 0;
+// for (const salary of Object.values(salaries)) {
+//   sum += salary;
+// }
+// console.log("загальна сумма", sum);
+
+// Напишіть функцію calcTotalPrice(fruits, fruitName),
+// яка приймає массив об'єктів (fruits) і рядок з назвою фрукта (fruitName).
+// Функція рахує і повертає загальну вартість фрукта
+// з таким ім'ям, ціною та кількістю з об'єкта.
+
+// Зверніть увагу, що в масиві може бути кілька обʼєктів з однаковою
+// назвою фрукта, це також треба урахувати.
+
+// const fruits = [
+//   { name: "Яблуко", price: 45, quantity: 7 },
+//   { name: "Апельсин", price: 60, quantity: 4 },
+//   { name: "Банан", price: 125, quantity: 8 },
+//   { name: "Груша", price: 350, quantity: 2 },
+//   { name: "Виноград", price: 440, quantity: 3 },
+//   { name: "Банан", price: 125, quantity: 3 },
+// ];
+
+// function calcTotalPrice(fruits, fruitName) {
+//   let total = 0;
+//   for (const fruit of fruits) {
+//     if (fruit.name === fruitName) {
+//       total += fruit.price * fruit.quantity;
+//     }
+//   }
+//   return total;
+// }
+// console.log(calcTotalPrice(fruits, "Банан"));
+// console.log(calcTotalPrice(fruits, "Яблуко"));
+
+// Напишіть функцію, яка сумуватиме сусідні числа
+// і пушитиме їх в новий масив.
+
+// const someArr = [22, 11, 34, 5, 12, 13, 14, 15];
+
+// уточнення: складати необхідно перше число з другим, потім друге - з третім,
+// третє - з четвертим і так до кінця.
+// В результаті функція має повертати масив [33, 45, 39, 17, 25, 27, 29].
+
+// function sumArray(arr) {
+//   const result = [];
+
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     result.push(arr[i] + arr[i + 1]);
+//   }
+//   return result;
+// }
+
+// console.log(sumArray(someArr));
+
+// Напишіть функцію findSmallestNumber(numbers),
+// яка шукає найменше число в масиві.
+// Додайте перевірку, що функція отримує саме масив, і
+// якщо функція отримує масив - поверніть з функції найменше число,
+// в іншому випадку - поверніть 'Sory, it is not an array!'.
+
+// const numbers = [80, 5, 35, 56, 12, 24, 7, 80, 3];
+
+// function findSmallestNumber(numbers) {
+//   if (!Array.isArray(numbers)) {
+//     return "Sory, it is not an array!";
+//   }
+//   let min = numbers[0];
+//   for (let i = 1; i < numbers.length; i++) {
+//     if (numbers[i] < min) {
+//       min = numbers[i];
+//     }
+//   }
+//   return min;
+// }
+
+// console.log(findSmallestNumber(numbers));
+
+// Напишіть функцію findLongestWord(string), яка
+// приймає довільний рядок, що складається лише зі слів, розділених
+// пробілами (параметр string), і повертатиме найдовше слово у реченні.
+
+// Скористайтесь цим прикладом виклику функції для перевірки її роботи:
+
+// function findLongestWord(string) {
+//   const words = string.split(" ");
+//   let longest = words[0];
+//   for (let i = 1; i < words.length; i++) {
+//     if (longest.length < words[i].length) {
+//       longest = words[i];
+//     }
+//   }
+//   return longest;
+// }
+// console.log(findLongestWord("London is the capital of Great Britain")); // 'capital'
+
+// Створіть об'єкт calculator з наступними методами:
+// read(a, b) - приймає два аргумента і зберігає їх як властивості об'єкта,
+// sum() - повертає сумму збереженних значень (з перевіркою на наявність властивостей в об'єкті),
+// mult() - перемножає збереженні значення і повертає результат (з перевіркою на наявність властивостей в об'єкті),
+// винесіть перевірку на наявність властивостей в об'єкті в окремий метод exist().
+
+// Якщо вказані властивості в обʼєкті відсутні (тобто метод exist повертає false),
+// методи sum і mult мають повертати рядок 'No such propeties'
+
+// const calculator = {
+//   a: null,
+//   b: null,
+//   read(a, b) {
+//     this.a = a;
+//     this.b = b;
+//   },
+//   exist() {
+//     return this.a !== null && this.b !== null;
+//   },
+//   sum() {
+//     if (this.exist()) {
+//       return this.a + this.b;
+//     }
+//     return "No such propeties";
+//   },
+//   mult() {
+//     if (this.exist()) {
+//       return this.a * this.b;
+//     }
+//     return "No such propeties";
+//   },
+// };
+
+// calculator.read(10, 5);
+// console.log(calculator.sum());
+// console.log(calculator.mult());
