d2501fb544f814e8b319514cc018cf221e754014
yurii-bezus <bylchenok@gmail.com>
2025-05-16 13:23:29 +0300

    up

---
+++ js/blended1/lesson-3.js
@@ -0,0 +1,95 @@
+// Дано масив об'єктів. Створіть новий масив, що містить всі значення
+// з масивів values кожного об'єкту, збережених в одному масиві.
+// Очікуваний результат: [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+// const data = [
+//   { id: 1, values: [1, 2, 3] },
+//   { id: 2, values: [4, 5, 6] },
+//   { id: 3, values: [7, 8, 9] },
+// ];
+
+// const allValues = data.flatMap((value) => value.values);
+
+// console.log(allValues);
+
+// Дано масив чисел [1, 2, 3, 4, 5]. Створіть новий масив, що містить
+// квадрати кожного елементу вхідного масиву.
+// Очікуваний результат: [1, 4, 9, 16, 25].
+
+// const numbers = [1, 2, 3, 4, 5];
+// const square = numbers.map((number) => number * number);
+
+// console.log(square);
+
+// Дано масив об'єктів.
+// Перевірте, чи є хоча б один об'єкт з віком менше 20 років.
+// Очікуваний результат: true.
+
+// const people = [
+//   { name: "John", age: 27 },
+//   { name: "Jane", age: 31 },
+//   { name: "Bob", age: 19 },
+// ];
+
+// const hasYoungPerson = people.some((people) => people.age < 20);
+
+// console.log(hasYoungPerson);
+
+// Дано масив чисел [2, 4, 6, 8, 10].
+// Перевірте, чи є кожен елемент масиву парним. Очікуваний результат: true.
+
+// const numbers = [2, 4, 6, 8, 10];
+
+// const everyEven = numbers.every((number) => number % 2 === 0);
+
+// console.log(everyEven);
+
+// Знайдіть перше непарне число
+
+// const numbers = [2, 1, 6, 8, 9, 10, 12];
+
+// const firstNumber = numbers.find((number) => number % 2 !== 0);
+
+// console.log(firstNumber);
+
+// Відсортуйте масив чисел [4, 2, 5, 1, 3]
+// у порядку зростання. Очікуваний результат: [1, 2, 3, 4, 5].
+
+// const numbersArray = [4, 2, 5, 1, 3];
+
+// const sortedArray = numbersArray.sort((a, b) => a - b);
+
+// console.log(sortedArray);
+
+// Відсортуйте масив рядків ["banana", "orange", "apple", "pear"]
+//  у порядку алфавіту.
+// Очікуваний результат: ["apple", "banana", "orange", "pear"].
+
+// const stringArray = ["banana", "orange", "apple", "pear"];
+
+// const sortedArray = stringArray.sort((a, b) => a.localeCompare(b));
+
+// console.log(sortedArray);
+
+// Дано масив об'єктів.
+// Створіть новий масив, що містить тільки об'єкти, в яких
+// вік більше 20 років.
+// Очікуваний результат: [{name: "John", age: 27}, {name: "Jane", age: 31}]
+
+// const user = [
+//   { name: "John", age: 27 },
+//   { name: "Jane", age: 31 },
+//   { name: "Bob", age: 19 },
+// ];
+
+// const filteredUsers = user.filter((user) => user.age > 20);
+
+// console.log(filteredUsers);
+
+// Дано масив чисел [1, 2, 3, 4, 5].
+// Застосуйте метод для обчислення суми елементів масиву.
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// const sum = numbers.reduce((acc, num) => acc + num, 0);
+// console.log(sum);
